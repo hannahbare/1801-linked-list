d94378ea1b1cd422300db761276ff7b91b0e8dc7
Hannah Bare <johnd>
2018-02-04 13:35:36 -0700

    Complete bookmark append function

--- scripts.js
+++ scripts.js
@@ -2,26 +2,19 @@ var userInputTitle = document.querySelector('#input-title');
 var userInputUrl = document.querySelector('#input-url');
 var userInputSubmit = document.querySelector('#input-submit');
 var bookmarkSection = document.querySelector('.cards');
-var bookmarkTitle = document.querySelector('h2');
-/*var bookmarkUrl = document.querySelector('.card__url');
-var bookmarkReadBtn = document.querySelector('#card__url--left');
-var bookmardDeleteBtn = document.querySelector('#card__url--right');*/
 
 userInputSubmit.addEventListener('click', appendBookmark);
 
-function appendBookmark () {
-  var inputTitle = userInputTitle.value;
-  bookmarkSection.appendChild(userInputTitle);
-
-
-`<h2>${userInputTitle.value}</h2> 
-   <article class="card__url">
-     <a href="www.thewebsiteurl.com" class="card-hover" id="card__url--link">www.thewebsiteurl.com</a>
-   </article>
-
-   <article class="cards__read-delete">
-     <a href="#" class="card-hover" id="card__url--left">Read</a>
-     <a href="#" id="card__url--right">Delete</a>
-   </article>`
-   console.log(userInputTitle.value)
+function appendBookmark() {
+  var newArticle = document.createElement('article');
+  bookmarkSection.appendChild(newArticle);
+  newArticle.innerHTML = 
+    `<h2>${userInputTitle.value}</h2> 
+     <p class="card__url">
+       <a href="#" class="card-hover" id="card__url--link">${userInputUrl.value}</a>
+     </p>
+     <p class="cards__read-delete">
+       <a href="#" class="card-hover" id="card__url--left">Read</a>
+       <a href="#" id="card__url--right">Delete</a>
+     </p>`
 }
