403b1d857dc4484231c8b96d3b3a65680fea763e
Lee Graham <graham.la3@gmail.com>
2018-02-07 17:40:34 -0700

    Reorganize functionality of submit button disable and alert event.

--- scripts.js
+++ scripts.js
@@ -3,44 +3,45 @@ var userInputUrl = document.querySelector('#input-url');
 var submitBtn = document.querySelector('#input-submit');
 var bookmarkSection = document.querySelector('.cards');
 
-userInputUrl.addEventListener('keyup', enableSubmitBtn);
-submitBtn.addEventListener('click', alertOrAppend);
+userInputUrl.addEventListener('keyup', enableBtn);
+userInputTitle.addEventListener('keyup', enableBtn);
+submitBtn.addEventListener('click', appendBookmark);
 bookmarkSection.addEventListener('click', toggleRead);
 bookmarkSection.addEventListener('click', deleteCard);
 
-function enableSubmitBtn(event) {
-  if (userInputTitle.value && userInputUrl.value) {
+
+function enableBtn() {
+  if (userInputTitle.value.length > 0 || userInputUrl.value.length > 0) {
     submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
   }
 }
 
-function alertOrAppend(event){
-  if (userInputTitle.value && userInputUrl.value) {
-    appendBookmark();
-    submitBtn.disabled = true;
-  } else if (userInputTitle.value === "" || userInputUrl.value === "") {
-    alert('Please fill out both input fields.');
-  } 
-}
-
 function appendBookmark() {
-  var newArticle = document.createElement('article');
-  newArticle.innerHTML = 
-    `<h2>${userInputTitle.value}</h2> 
-     <p class="card__url">
-       <a href="${userInputUrl.value}" class="card-hover" id="card__url--link" target="_blank">${userInputUrl.value}</a>
-     </p>
-     <p class="cards__read-delete">
-       <a href="#" class="card-hover read-btn" id="card__url--read">Read</a>
-       <a href="#" id="card__url--delete">Delete</a>
-     </p>`
-  bookmarkSection.appendChild(newArticle);
-  clearInputFields();
+  if (userInputTitle.value.length > 0 && userInputUrl.value.length > 0) {
+    var newArticle = document.createElement('article');
+    newArticle.innerHTML = 
+      `<h2>${userInputTitle.value}</h2> 
+       <p class="card__url">
+         <a href="${userInputUrl.value}" class="card-hover" id="card__url--link" target="_blank">${userInputUrl.value}</a>
+       </p>
+       <p class="cards__read-delete">
+         <a href="#" class="card-hover read-btn" id="card__url--read">Read</a>
+         <a href="#" id="card__url--delete">Delete</a>
+       </p>`
+    bookmarkSection.appendChild(newArticle);
+    submitBtn.disabled = true;
+    clearInputFields();
+    } else {
+      alert('Please fill out both input fields.');
+      submitBtn.disabled = true;
+    }
 }
 
 function clearInputFields(){
-  userInputTitle.value = " ";
-  userInputUrl.value = " ";
+  userInputTitle.value = "";
+  userInputUrl.value = "";
 }
 
 function toggleRead(event){
